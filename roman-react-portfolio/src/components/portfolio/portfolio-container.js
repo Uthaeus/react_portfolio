7a25b1f80866a39f385cd269d14f59f57a2daf93
Uthaeus <romanlavery@gmail.com>
2019-11-15 11:02:55 -0700

    working with state

--- roman-react-portfolio/src/components/portfolio/portfolio-container.js
+++ roman-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -5,20 +5,29 @@ import PortfolioItem from './portfolio-item';
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip"}, 
+                {title: "Eventbrite"}, 
+                {title: "Ministry Safe"}, 
+                {title: "SwingAway"}
+            ]
+        };
     }
 
     portfolioItems() {
-        const data = ["Quip", "Evenbright", "Ministry Safe", "SwingAway"];
 
-        return data.map(item => {
-            return <PortfolioItem tite={item} url={"google.com"} />;
+        return this.state.data.map(item => {
+            return <PortfolioItem tite={item.title} url={"google.com"} />;
         })
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
                 { this.portfolioItems() }
             </div>
         )
