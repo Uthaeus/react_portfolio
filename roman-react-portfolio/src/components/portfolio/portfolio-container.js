428834a0ae626ad0ceba6390cebf3b1af9b995df
Uthaeus <romanlavery@gmail.com>
2019-11-15 10:40:48 -0700

    passing props

--- roman-react-portfolio/src/components/portfolio/portfolio-container.js
+++ roman-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+    }
+
+    portfolioItems() {
+        const data = ["Quip", "Evenbright", "Ministry Safe", "SwingAway"];
+
+        return data.map(item => {
+            return <PortfolioItem tite={item} url={"google.com"} />;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
-
-                <PortfolioItem />
+                { this.portfolioItems() }
             </div>
         )
     }
