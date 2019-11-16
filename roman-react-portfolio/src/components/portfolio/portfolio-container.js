ece3d18fccf86a987d78737dfaa8dd24fc6db52c
Uthaeus <romanlavery@gmail.com>
2019-11-16 16:40:56 -0700

    using key prop for portfolio items

--- roman-react-portfolio/src/components/portfolio/portfolio-container.js
+++ roman-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -41,7 +41,7 @@ export default class PortfolioContainer extends Component {
 
         return this.state.data.map(item => {
             console.log('item data', item);
-            return <PortfolioItem tite={item.name} url={item.url} slug={item.id} />;
+            return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
         })
     }
 
