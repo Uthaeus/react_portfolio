e07522a15c0b783f5b8df2c70b1d2911b9459b1f
Uthaeus <romanlavery@gmail.com>
2019-11-17 10:36:26 -0700

    implementing flexbox for nav

--- roman-react-portfolio/src/components/navigation/navigation-container.js
+++ roman-react-portfolio/src/components/navigation/navigation-container.js
@@ -8,14 +8,20 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div>
-                <NavLink exact to='/'>Home</NavLink>
-                <NavLink to='/about-me'>About</NavLink>
-                <NavLink to='/contact'>Contact</NavLink>
-                <NavLink to='/blog'>Blog</NavLink>
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <NavLink exact to='/'>Home</NavLink>
+                    <NavLink to='/about-me'>About</NavLink>
+                    <NavLink to='/contact'>Contact</NavLink>
+                    <NavLink to='/blog'>Blog</NavLink>
 
-                
-                { false ? <button>Add Blog</button> : null }
+                    
+                    { false ? <button>Add Blog</button> : null }
+                </div>
+
+                <div className="right-side">
+                    Homer Simpson
+                </div>
             </div>
         )
     }
