057cd8aa448e165d00ef8a9b774d8f0edb88761c
Uthaeus <romanlavery@gmail.com>
2019-11-18 08:59:11 -0700

    importing static image for auth page

--- roman-react-portfolio/src/components/pages/auth.js
+++ roman-react-portfolio/src/components/pages/auth.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 
+import loginImage from '../../../static/assets/images/auth/login.jpg';
+
 export default class Auth extends Component {
-    
+
+    render() {
+        return (
+            <div className="auth-page-wrapper">
+                <div className="left-column" 
+                    style={{
+                        backgroundImage: `url(${loginImage})`
+                    }} />
+
+                <div className="right-column">
+                    <h2>Login component here</h2>
+                </div>
+            </div>
+        );
+    }
 }
