0558cee141831aeec53575ef907434939fdadb9e
Uthaeus <romanlavery@gmail.com>
2019-11-19 09:14:43 -0700

    showing links when logged in

--- roman-react-portfolio/src/components/navigation/navigation-container.js
+++ roman-react-portfolio/src/components/navigation/navigation-container.js
@@ -3,6 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 
 const NavigationComponent = props => {
+
+    const dynamicLink = (route, linkText) => {
+        return (
+            <div className="nav-link-wrapper">
+                <NavLink to='/blog'>Blog</NavLink>
+            </div>
+        )
+    }
+
     return (
         <div className="nav-wrapper">
             <div className="left-side">
@@ -15,10 +24,8 @@ const NavigationComponent = props => {
                 <div className="nav-link-wrapper">
                     <NavLink to='/contact'>Contact</NavLink>
                 </div>
-                <div className="nav-link-wrapper">
-                    <NavLink to='/blog'>Blog</NavLink>
-                </div>
-
+                
+                { props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null }
                 
                 { false ? <button>Add Blog</button> : null }
             </div>
