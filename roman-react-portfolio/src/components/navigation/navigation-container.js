9975d799aab8218e66e547cacd76abc24acead4b
Uthaeus <romanlavery@gmail.com>
2019-11-19 09:05:42 -0700

    refactor class component to functional component

--- roman-react-portfolio/src/components/navigation/navigation-container.js
+++ roman-react-portfolio/src/components/navigation/navigation-container.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
 
-    render() {
-        return (
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to='/'>Home</NavLink>
-                    </div>
-                    <div className="nav-link-wrapper">
-                        <NavLink to='/about-me'>About</NavLink>
-                    </div>
-                    <div className="nav-link-wrapper">
-                        <NavLink to='/contact'>Contact</NavLink>
-                    </div>
-                    <div className="nav-link-wrapper">
-                        <NavLink to='/blog'>Blog</NavLink>
-                    </div>
-
-                    
-                    { false ? <button>Add Blog</button> : null }
+const NavigationComponent = props => {
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to='/'>Home</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/about-me'>About</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/contact'>Contact</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to='/blog'>Blog</NavLink>
                 </div>
 
-                <div className="right-side">
-                    Homer Simpson
-                </div>
+                
+                { false ? <button>Add Blog</button> : null }
             </div>
-        )
-    }
+
+            <div className="right-side">
+                Homer Simpson
+            </div>
+        </div>
+    )
 }
+
+export default NavigationComponent;
