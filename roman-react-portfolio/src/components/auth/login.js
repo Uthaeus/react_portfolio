a79fc32aef29d74f6b1218c743ffeb7ab280ae49
Uthaeus <romanlavery@gmail.com>
2019-11-18 19:58:57 -0700

    updating parent component state from a child component

--- roman-react-portfolio/src/components/auth/login.js
+++ roman-react-portfolio/src/components/auth/login.js
@@ -25,16 +25,18 @@ export default class Login extends Component {
             { withCredentials: true }
         ).then(response => {
             if (response.data.status === 'created') {
-
+                this.props.handleSuccessfulAuth();
             } else {
                 this.setState({
                     errorText: "Wrong email or password"
-                })
+                });
+                this.props.handleUnsuccessfulAuth();
             }
         }).catch(error => {
             this.setState({
                 errorText: "An error occurred"
-            })
+            });
+            this.props.handleUnsuccessfulAuth();
         });
         
 
