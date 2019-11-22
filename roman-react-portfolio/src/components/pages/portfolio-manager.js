39c441e7dc5d2467cdc437996688f0352232f011
Uthaeus <romanlavery@gmail.com>
2019-11-22 09:48:15 -0700

    portfolio manager grid layout

--- roman-react-portfolio/src/components/pages/portfolio-manager.js
+++ roman-react-portfolio/src/components/pages/portfolio-manager.js
@@ -5,8 +5,14 @@ export default class PortfolioManager extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Portfolio Manager</h1>
+            <div className="portfolio-manager-wrapper">
+                <div className="left-column">
+                    <h1>form</h1>
+                </div>
+
+                <div className="right-column">
+                    <h1>sidebar</h1>
+                </div>
             </div>
         );
     }
